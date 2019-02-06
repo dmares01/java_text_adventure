615d530abc3ff6bbe47442537b5d1dcaad0737be
Drew Mares <mikkelmail98@gmail.com>
2019-02-05 19:12:39 -0600

    Added simple beginning to main class

--- src/rpg_adventure/main.java
+++ src/rpg_adventure/main.java
@@ -1,4 +1,12 @@
 package rpg_adventure;
 
 public class main {
+    public static void main(String [] args){
+        int health = 10;
+        double money = 20.0;
+        String name = " ";
+
+        System.out.println("Welcome to Defiled Dungeons");
+
+    }
 }
